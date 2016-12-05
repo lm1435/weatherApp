305c2a16d0c403a02fbe0a8a41263c3c2d9ebca8
Luis Meza <luismeza01@gmail.com>
2016-12-04 22:21:09 -0600

    added some styling and bourbon

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,9 @@
 $(document).ready(function(){
-  var long;
-  var lat;
-  var farenheit;
-  var celsius;
+  var tempChange=true;
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position){
       long= position.coords.longitude;
       lat= position.coords.latitude;
-
 
       var api = "http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+long+"&appid=ec5b09b3c6f823a773734e69baa839d4";
 
@@ -16,15 +12,40 @@ $(document).ready(function(){
         var kelvin = data.main.temp;
         var windSpeed = data.wind.speed;
         var city = data.name;
+        var humidity = data.main.humidity;
 
         farenheit = (kelvin)*(9/5)-459.67;
 
         celsius = kelvin - 273.15;
 
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth()+1;
+        var yyyy = today.getFullYear();
+
+        if(dd<10) {
+            dd='0'+dd;
+        }
+        if(mm<10) {
+            mm='0'+mm;
+        }
+        today = mm+' / '+dd+' / '+yyyy;
+        $('#date').html(today);
+
         $('#temp').html(farenheit);
+        $('#temp').click(function(){
+          if(tempChange==false) {
+            $('#temp').html(celsius "&#8451;");
+            tempChange=true;
+          } else {
+            $('#temp').html(farenheit);
+            tempChange=false;
+          }
+        });
         $('#city').html(city);
-        $('#wind').html(windSpeed);
-        $('#description').html(weatherType);
+        $('#windSpeed').html(windSpeed);
+        $('#weatherType').html(weatherType);
+        $('#humidity').html(humidity);
       });
     });
   }
